927674ee9045d8e9e5bbc6091c2780281d557392
dev4 <programacao4@eagledigital.com.br>
2017-05-25 10:06:21 -0300

    added missing model functions

--- inc/database.php
+++ inc/database.php
@@ -22,32 +22,105 @@ function close_database($conn)
     }
 }
 
-/*
- * Preencha as funções abaixo;
- * Pode alterar os parametros se preferir;
-*/
-
 function find( $table = null, $id = null ) 
 {  
-    // stuff
+    $database = open_database();
+    $found = null;
+    try {
+        if ($id) {
+            $sql = "SELECT * FROM " . $table . " WHERE id = " . $id;
+            $result = $database->query($sql);
+
+            if ($result->num_rows > 0) {
+                $found = $result->fetch_assoc();
+            }
+        } else {
+            $sql = "SELECT * FROM " . $table;
+            $result = $database->query($sql);
+
+            if ($result->num_rows > 0) {
+                $found = $result->fetch_all(MYSQLI_ASSOC);
+            }
+        }
+    } catch (Exception $e) {
+        $_SESSION['message'] = $e->GetMessage();
+        $_SESSION['type'] = 'danger';
+    }
+
+    close_database($database);
+    return $found;
 }
 
 function find_all( $table ) 
 {
-    // stuff
+    return find($table);
 }
 
 function save($table = null, $data = null) 
 {
-    // stuff
+    $database = open_database();
+    $columns = null;
+    $values = null;
+    //print_r($data);
+    foreach ($data as $key => $value) {
+        $columns .= trim($key, "'") . ",";
+        $values .= "'$value',";
+    }
+    // remove a ultima virgula
+    $columns = rtrim($columns, ',');
+    $values = rtrim($values, ',');
+
+    $sql = "INSERT INTO " . $table . "($columns)" . " VALUES " . "($values);";
+    try {
+        $database->query($sql);
+        $_SESSION['message'] = 'Registro cadastrado com sucesso.';
+        $_SESSION['type'] = 'success';
+    } catch (Exception $e) { 
+        $_SESSION['message'] = 'Nao foi possivel realizar a operacao.';
+        $_SESSION['type'] = 'danger';
+    } 
+    close_database($database);
 }
 
 function update($table = null, $id = 0, $data = null) 
 {
-    // stuff
+    $database = open_database();
+    $items = null;
+    foreach ($data as $key => $value) {
+        $items .= trim($key, "'") . "='$value',";
+    }
+    // remove a ultima virgula
+    $items = rtrim($items, ',');
+    $sql  = "UPDATE " . $table;
+    $sql .= " SET $items";
+    $sql .= " WHERE id=" . $id . ";";
+    try {
+        $database->query($sql);
+        $_SESSION['message'] = 'Registro atualizado com sucesso.';
+        $_SESSION['type'] = 'success';
+    } catch (Exception $e) { 
+        $_SESSION['message'] = 'Nao foi possivel realizar a operacao.';
+        $_SESSION['type'] = 'danger';
+    } 
+    close_database($database);
 }
 
 function remove( $table = null, $id = null ) 
 {
-    // stuff    
+    $database = open_database();
+
+    try {
+        if ($id) {
+            $sql = "DELETE FROM " . $table . " WHERE id = " . $id;
+            $result = $database->query($sql);
+            if ($result = $database->query($sql)) {       
+                $_SESSION['message'] = "Registro Removido com Sucesso.";
+                $_SESSION['type'] = 'success';
+            }
+        }
+    } catch (Exception $e) { 
+        $_SESSION['message'] = $e->GetMessage();
+        $_SESSION['type'] = 'danger';
+    }
+    close_database($database);  
 }
